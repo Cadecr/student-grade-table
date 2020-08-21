6900f91f1628c2e33dde4fdbebdae28edd303e20
cadecr <cadecr@gmail.com>
2020-08-20 20:05:40 -0700

    Requesting Data

---
+++ Components/app.js
@@ -0,0 +1,67 @@
+class App {
+  constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+
+  handleGetGradesError(error) {
+    console.error(error);
+  }
+
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  }
+
+  getGrades() {
+    $.ajax({
+      type: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      "headers": {
+        "X-Access-Token": "KyjK1372"
+      },
+      response: [
+      {
+        "id": 15,
+        "name": "Tim Davis",
+        "course": "Web Development",
+        "grade": 98,
+        "createdBy": "Joe Bishop"
+      },
+      {
+        "id": 16,
+        "name": "Cody Miller",
+        "course": "Web Development",
+        "grade": 98,
+        "createdBy": "Joe Bishop"
+      },
+      {
+        "id": 17,
+        "name": "Tim Horist",
+        "course": "Web Development",
+        "grade": 98,
+        "createdBy": "Joe Bishop"
+      },
+      {
+        "id": 18,
+        "name": "Brett Albright",
+        "course": "Web Development",
+        "grade": 98,
+        "createdBy": "Joe Bishop"
+      },
+      {
+        "id": 19,
+        "name": "Scott Bowler",
+        "course": "Web Development",
+        "grade": 98,
+        "createdBy": "Joe Bishop"
+      }
+    ],
+      success: this.handleGetGradesSuccess(),
+      error: this.handleGetGradesError()
+  });
+  }
+
+  start() {
+    this.getGrades();
+  }
+}
---
+++ main.js
@@ -0,0 +1,2 @@
+var initiate = new App;
+initiate.start();
