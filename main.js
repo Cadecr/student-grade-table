1d4e95250b4f3db34879dc243c462eae8291291c
cadecr <cadecr@gmail.com>
2020-08-22 12:14:57 -0700

    declared the header variable in main.js as jQuery for <h5> which is the Average Grade element. Declared the variable pageHeader as a new PageHeader class and passed in the header variable.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var tableBody = $("<table>");
+var header = $("<h5>");
+var pageHeader = new PageHeader(header);
 var newTable = new GradeTable(tableBody);
 var initiate = new App(newTable);
 initiate.start();
