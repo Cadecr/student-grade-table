4ade9467ed5f2130b6b7404e9e1f590629c8106b
cadecr <cadecr@gmail.com>
2020-08-22 12:16:15 -0700

    modified the initiate variable in main.js and included the pageHeader variable to pass in along with newTable for the new App.

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var tableBody = $("<table>");
 var header = $("<h5>");
 var pageHeader = new PageHeader(header);
 var newTable = new GradeTable(tableBody);
-var initiate = new App(newTable);
+var initiate = new App(newTable, pageHeader);
 initiate.start();
