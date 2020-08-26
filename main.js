00cfe2b99dd1b20b0816541d99ada62359ef2c35
cadecr <cadecr@gmail.com>
2020-08-25 19:25:56 -0700

    queried the document to the form element and instantiated the Grade Form class with assigning it to the gradeForm variable and passed in my form variable as an argument

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var tableBody = $("<table>");
 var header = document.querySelector("#header");
+var form = document.querySelector("<form>")
+var gradeForm = new GradeForm(form);
 var pageHeader = new PageHeader(header);
 var newTable = new GradeTable(tableBody);
 var initiate = new App(newTable, pageHeader);
