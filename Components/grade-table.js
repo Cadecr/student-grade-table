9359db67484f4eb0e8a0c5907aa10fbe07273757
cadecr <cadecr@gmail.com>
2020-08-27 18:59:24 -0700

    enhanced the updateGrades method to use this.renderGradeRow and passed in grades[i] and the this.deleteGrade callback.

--- Components/grade-table.js
+++ Components/grade-table.js
@@ -8,17 +8,18 @@ class GradeTable {
     var tBody = $("#table");
     tBody.empty();
     for (var i = 0; i < grades.length; i++) {
-      var row = document.createElement("tr");
-      var studentName = document.createElement("td");
-      var course = document.createElement("td");
-      var grade = document.createElement("td");
-      tBody.append(row);
-      row.appendChild(studentName);
-      studentName.textContent = grades[i].name;
-      row.appendChild(course);
-      course.textContent = grades[i].course;
-      row.appendChild(grade);
-      grade.textContent = grades[i].grade;
+      // var row = document.createElement("tr");
+      // var studentName = document.createElement("td");
+      // var course = document.createElement("td");
+      // var grade = document.createElement("td");
+      // tBody.append(row);
+      // row.appendChild(studentName);
+      // studentName.textContent = grades[i].name;
+      // row.appendChild(course);
+      // course.textContent = grades[i].course;
+      // row.appendChild(grade);
+      // grade.textContent = grades[i].grade;
+      this.renderGradeRow(grades[i], this.deleteGrade);
     }
   }
 
@@ -44,6 +45,6 @@ class GradeTable {
     row.appendChild(deleteData);
     deleteData.appendChild(button);
     button.textContent = "DELETE";
-
+    return row;
   }
 }
